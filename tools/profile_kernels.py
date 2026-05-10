e0642f75e6f70a436bd6b58fc682e800d68bd568
Jithin Njondimackal <njondimackal.jithin@gmail.com>
2026-05-10 13:14:32 +0200

    Added python script for profiling design space for different kernels

---
+++ tools/profile_kernels.py
@@ -0,0 +1,50 @@
+import subprocess
+
+KERNEL_LIST = [
+    ("sgemm_small", 1),
+    ("sgemm_medium", 2),
+    ("sgemm_large", 3),
+    ("sgemm_tall", 4),
+    ("sgemm_wide", 5),
+    ("sgemm_huge", 6),
+    ("ft_sgemm_small", 11),
+    ("ft_sgemm_medium", 12),
+    ("ft_sgemm_large", 13),
+    ("ft_sgemm_tall", 14),
+    ("ft_sgemm_wide", 15),
+    ("ft_sgemm_huge", 16),
+    ("ft_sgemm_medium_96", 17),
+]
+
+KERNEL_SIZES = [
+    1024,
+    2048,
+    3072,
+    4096,
+    5120,
+    6144,
+    7168,
+    8192,
+]
+
+NCU_PATH = "/opt/nvidia/nsight-compute/2024.3.1/target/linux-v4l_l4t-t210-a64/ncu"
+FT_SGEMM_PATH = "/home/jithin/repos/Fault-Tolerant-SGEMM-on-NVIDIA-GPUs/build"
+NCU_RESULTS_PATH = f"{FT_SGEMM_PATH}/ncu_results"
+
+if __name__ == "__main__":
+    for kernel_name, kernel_id in KERNEL_LIST:
+        print(f"Profiling {kernel_name} kernel...")
+
+        for size in KERNEL_SIZES:
+            print(f"Profiling {kernel_name} with size {size}...")
+
+            ncu_cmd = f"{NCU_PATH} --config-file off \
+            --export {NCU_RESULTS_PATH}/ncu_{kernel_name}_{size} \
+            --force-overwrite \
+            --kernel-name {kernel_name} \
+            {FT_SGEMM_PATH}/ft_sgemm {size} {size} {size+1} {kernel_id} {kernel_id}"
+            print(ncu_cmd)
+
+            subprocess.run(ncu_cmd, shell=True)
+
+        print(f"Finished profiling {kernel_name} kernel.")
